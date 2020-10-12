60f07b7e5dbf8d842c083f838d0995322a4077c9
Ruff Rider <49044157+bowen227@users.noreply.github.com>
2020-10-11 21:35:49 -0400

    Adding new blog post.

--- src/app/apps/blog-app/blog-app.component.ts
+++ src/app/apps/blog-app/blog-app.component.ts
@@ -17,8 +17,9 @@ export class BlogAppComponent implements OnInit {
       `<h2>Fat to fit</h2>
       <p>My personal journey</p>
       <p>
-      A few years ago I was one of the millions of people who had chronic back pain and while I didn't think I was that overweight soon realized I was. My back would go out nearly
-      every few weeks which would put me done for a few days each time.
+      My name is Brent Owen and it wasn't that long ago I was 70 pounds heabier and a sufferer of chronic back pain. My back would go out every few weeks doing super hard task
+      like picking up a dropped piece of paper. Ok, really simple task. I'd be down for a few days and finally push through with the help of muscle relaxers to get back to everyday
+      things. I went to lots fo doctors, physical therapist and had more than enought shots in my back.
       </p>
       
       `

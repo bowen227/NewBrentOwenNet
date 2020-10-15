65dc72ae70a908a730ec6e666f0157d22c60f172
bowen227 <owen.brent@gmail.com>
2020-10-14 20:35:24 -0400

    Added firebase config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCU-pcCLDccrMGBMGwyW-HExAtq9u62m-M",
+    authDomain: "blog-app-ac2af.firebaseapp.com",
+    databaseURL: "https://blog-app-ac2af.firebaseio.com",
+    projectId: "blog-app-ac2af",
+    storageBucket: "blog-app-ac2af.appspot.com",
+    messagingSenderId: "845270458448",
+    appId: "1:845270458448:web:50bd06c164c549fb317155",
+    measurementId: "G-8MVXJZB3DB"
+  }
 };

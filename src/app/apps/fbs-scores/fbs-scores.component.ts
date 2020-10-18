20f990bf56e11c8884f01f2f6f56a976bccd0f49
Ruff Rider <49044157+bowen227@users.noreply.github.com>
2020-10-17 22:03:43 -0400

    Changing to rxjs observables. Not completed.

--- src/app/apps/fbs-scores/fbs-scores.component.ts
+++ src/app/apps/fbs-scores/fbs-scores.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { map, subscribeOn } from 'rxjs/operators';
+import { Observable, from, observable } from 'rxjs';
 import { Game } from './game';
-
 
 
 @Component({
@@ -14,14 +13,21 @@ import { Game } from './game';
 export class FbsScoresComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
-  public data: Observable<Game[]>;
+  public data;
   public week;
-  public keys = [];
+  public clock: Observable<string>;
   public names = [];
   public games = [];
   public gamesURL = "https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard";
 
   ngOnInit(): void {
+
+    this.sortData();
+    
+  }
+
+  public sortData() {
+    let newGames = [];
     this.getData().subscribe(data => {
       console.log(data);
       this.week = data['week'].number;
@@ -32,30 +38,41 @@ export class FbsScoresComponent implements OnInit {
             this.names.push(element);
           }
         }
+        this.names.map(item => {
+          item.map(i => {
+            if (i['competitions'][0]['situation']) {
+              let game: Game = {
+                date: i.date,
+                homeTeam: i['competitions'][0]['competitors'][0]['team'].abbreviation,
+                awayTeam: i['competitions'][0]['competitors'][1]['team'].abbreviation,
+                homeScore: i['competitions'][0]['competitors'][0].score,
+                awayScore: i['competitions'][0]['competitors'][1].score,
+                status: i['status']['type']['shortDetail'],
+                situation: i['competitions'][0]['situation']
+              }
+              newGames.push(game as Game);
+            }
+    
+            if (!i['competitions'][0]['situation']) {
+              let game: Game = {
+                date: i.date,
+                homeTeam: i['competitions'][0]['competitors'][0]['team'].abbreviation,
+                awayTeam: i['competitions'][0]['competitors'][1]['team'].abbreviation,
+                homeScore: i['competitions'][0]['competitors'][0].score,
+                awayScore: i['competitions'][0]['competitors'][1].score,
+                status: i['status']['type']['shortDetail'],
+                situation: null
+              }
+              newGames.push(game as Game);
+            }
+          });
+        });
       }
-      this.sortData();
     });
+    this.games = newGames;
   }
 
-  public sortData() {
-    this.names.map(item => {
-      item.map(i => {
-          let game: Game = {
-            date: i.date,
-            homeTeam: i['competitions'][0]['competitors'][0]['team'].abbreviation,
-            awayTeam: i['competitions'][0]['competitors'][1]['team'].abbreviation,
-            homeScore: i['competitions'][0]['competitors'][0].score,
-            awayScore: i['competitions'][0]['competitors'][1].score,
-            status: i['status']['type']['shortDetail'],
-            // SITUATION SOMETIMES BLANK NEED TO HANDLE THIS
-            situation: i['competitions'][0]['situation']['downDistanceText']
-          }
-          this.games.push(game as Game);
-      });
-    });
-  }
-
-  public getData() {
+  public getData(): Observable<any> {
     return this.http.get(this.gamesURL).pipe(map(res => res as JSON));
   }
 

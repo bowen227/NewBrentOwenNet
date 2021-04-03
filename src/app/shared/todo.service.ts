df45fdc0e93f468e43be3deec7b7cec3900371cf
Brent <brent.e.owen@gmail.com>
2021-04-02 20:46:00 -0400

    Fixed layout issue. Added social links to hero section. Added types to todo service functions for better readability.

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -15,46 +15,46 @@ export class TodoService {
   readonly TodosURI = "https://exerciselist.azurewebsites.net/api/Todo";
 
   // GET Group By User
-  public getGroupsByUser(user) {
+  public getGroupsByUser(user: string) {
     return this.http.get(this.GroupsURI + '/' + user).pipe(map(res => res as JSON));
   }
 
   // POST New Group
-  public addNewGroup(group) {
+  public addNewGroup(group: object) {
     return this.http.post(this.GroupsURI, group).pipe(map(res => res as JSON));
   }
 
   // PUT Group
-  public updateGroupName(id, nObj) {
+  public updateGroupName(id: string, nObj: object) {
     return this.http.put(`${this.GroupsURI}/${id}`, nObj, { headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })}).pipe(map(res => res as any));
   }
 
   // DELETE Group
-  public deleteGroupById(id) {
+  public deleteGroupById(id: string) {
     return this.http.delete(this.GroupsURI + '/' + id).pipe(map(res => res as JSON));
   }
 
   // GET Todo By User and Group
-  public getTodosByGroup(id, group) {
+  public getTodosByGroup(id: string, group: number) {
     return this.http.get(this.TodosURI + '/' + id + '/' + group).pipe(map(res => res as JSON));
   }
 
   // POST New Item
-  public addNewTodoToGroup(item) {
+  public addNewTodoToGroup(item: object) {
     return this.http.post(this.TodosURI, item).pipe(map(res => res as JSON));
   }
 
   // PUT Item
-  public updateItemName(todo) {
+  public updateItemName(todo: object) {
     return this.http.put(`${this.TodosURI}`, todo, { headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })}).pipe(map(res => res as any));
   }
 
   // DELETE Item
-  public deleteTodoById(id) {
+  public deleteTodoById(id: string) {
     return this.http.delete(this.TodosURI + '/' + id).pipe(map(res => res as JSON));
   }
 }

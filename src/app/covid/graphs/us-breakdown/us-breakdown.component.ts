e58934cd4acdc2f7b8501fd2d3d2b9de84c9a3cf
Brent <brent.e.owen@gmail.com>
2021-05-17 21:26:15 -0400

    Updated home layout. Fixed ustotals covid graph. API shut down saved copy of data to shared folder.

--- src/app/covid/graphs/us-breakdown/us-breakdown.component.ts
+++ src/app/covid/graphs/us-breakdown/us-breakdown.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CovidService } from 'src/app/shared/covid.service';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
+import { USTOTALS } from '../../../shared/daily';
 
 @Component({
   selector: 'app-us-breakdown',
   templateUrl: './us-breakdown.component.html',
-  styleUrls: ['./us-breakdown.component.css']
+  styleUrls: ['./us-breakdown.component.css'],
 })
 export class UsBreakdownComponent implements OnInit {
-  public usTotals;
+  public usTotals = USTOTALS;
   public l = [];
   public pos = [];
   public neg = [];
@@ -27,7 +28,7 @@ export class UsBreakdownComponent implements OnInit {
   public lineChartType = 'line';
   public lineChartOptions: { responsive: true };
 
-  constructor(private service: CovidService) { }
+  constructor(private service: CovidService) {}
 
   ngOnInit(): void {
     this.getUsTotals();
@@ -35,31 +36,43 @@ export class UsBreakdownComponent implements OnInit {
 
   public getUsTotals() {
     this.isLoading = true;
-    this.service.getUsaTotals()
-    .subscribe(data => {
-      console.log(data);
-      this.usTotals = data;
-      for (let i = 0; i < this.usTotals.length; i++) {
-        const element = this.usTotals[i];
-        this.l.push(String(element.date).slice(4, 8));
-        this.pos.push(element.positive);
-        this.neg.push(element.negative);
-        this.d.push(element.total);
-        this.t.push(element.totalTestResults);
-        this.h.push(element.hospitalizedCurrently);
-        this.died.push(element.death);
-      }
-      this.lineChartLabels = this.l.reverse();
-      this.newData.push({ data: this.pos.reverse(), label: "Positive" });
-      this.newData.push({ data: this.neg.reverse(), label: "Negative" });
-      // this.newData.push({ data: this.d.reverse(), label: "Total" });
-      // this.newData.push({ data: this.t.reverse(), label: "Total Tested" });
-      // this.newData.push({ data: this.h.reverse(), label: "Hospitalized" });
-      this.lineChartData = this.newData;
-      // console.log(this.lineChartData);
-      // console.log("Percent died US: " + this.died[0] / this.d[0] * 100);
-    });
+    for (let i = 0; i < this.usTotals.length; i++) {
+      const element = this.usTotals[i];
+      this.l.push(String(element.date).slice(4, 8));
+      this.pos.push(element.positive);
+      this.neg.push(element.negative);
+      this.d.push(element.total);
+      this.t.push(element.totalTestResults);
+      this.h.push(element.hospitalizedCurrently);
+      this.died.push(element.death);
+    }
+    this.lineChartLabels = this.l.reverse();
+    this.newData.push({ data: this.pos.reverse(), label: 'Positive' });
+    this.newData.push({ data: this.neg.reverse(), label: 'Negative' });
+    this.lineChartData = this.newData;
     this.isLoading = false;
   }
 
+  //   public getUsTotals() {
+  //     this.isLoading = true;
+  //     this.service.getUsaTotals()
+  //     .subscribe(data => {
+  //       this.usTotals = data;
+  //   for (let i = 0; i < this.usTotals.length; i++) {
+  //     const element = this.usTotals[i];
+  //     this.l.push(String(element.date).slice(4, 8));
+  //     this.pos.push(element.positive);
+  //     this.neg.push(element.negative);
+  //     this.d.push(element.total);
+  //     this.t.push(element.totalTestResults);
+  //     this.h.push(element.hospitalizedCurrently);
+  //     this.died.push(element.death);
+  //   }
+  //   this.lineChartLabels = this.l.reverse();
+  //   this.newData.push({ data: this.pos.reverse(), label: "Positive" });
+  //   this.newData.push({ data: this.neg.reverse(), label: "Negative" });
+  //   this.lineChartData = this.newData;
+  //     });
+  //     this.isLoading = false;
+  //   }
 }
